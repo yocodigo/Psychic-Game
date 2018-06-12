9a3bad47fcaf68926ddf2000a206de3223ef4cac
unknown <jjhernandez94@gmail.com>
2018-06-11 20:51:53 -0700

    visibility functionality in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,66 @@
 $(document).ready(function() {
-	function clickFunction(event) {
-		document.getElementById("display-4").innerHTML = "Enter any letter";
-		document.getElementsByClassName("game").style.visibility = "visible";
-	}	
-			
-		//Initial counts
-		let wCount = 0;
-		let lCount = 0;
-		let gCount = 9;
-		$("#winCount").html(wCount);
-		$("#lossCount").html(lCount);
-		$("#guessCount").html(gCount);
-		
-		let computerGuess = $("pcGuess");
+	var gameStatus = false;
 	
-		let userText = $("guess");
+	$(".jumbotron").click(function() {
+		gameStatus = true;
+		$("#display-4").html("Enter any letter");
 		
+	});	
+	debugger;
+	console.log(gameStatus);
+	if(gameStatus) {
+		runGame();
+	}
+	
+	//Initial counts
+	let wCount = 0;
+	let lCount = 0;
+	let gCount = 9;
+	let computerGuess = $("pcGuess");
+	let userText = $("guess");
+	
+	//Display initial counts
+	$("#winCount").html(wCount);
+	$("#lossCount").html(lCount);
+	$("#guessCount").html(gCount);
+		
+	function runGame() {
+
+		$(".game").attr("visibility", "visible");	
+
 		// User Selection Process
 		//When I press a button(onkeyup), trigger the following events (using function(event)...
 		document.onkeyup = function(event) {
-		userText.textContent = event.key;
-		let userGuess = event.key;
-		
-		//Computer Selection Process
-		let computerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-		let computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-		computerGuess.textContent;
-
-		//Compare user and computer selections
-	 	if (userGuess != computerGuess) {
 			
-			if (gCount <= 0) {
-				lCount++;
-				$("#lossCount").html(lCount);
-				gCount = 9;
-				$("#guessCount").html(gCount);
-				alert("YOU LOSE! Start Over.");
-			}else {
-				gCount--;
-				$("#guessCount").html(gCount);
+			userText.textContent = event.key;
+			let userGuess = event.key;
+			
+			//Computer Selection Process
+			let computerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+			let computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+			computerGuess.textContent;
+
+			//Compare user and computer selections
+			if (userGuess != computerGuess) {
 				
-			}	
-		}else {
-			alert("YOU WIN!");				
-			wCount++;
-			$("#winCount").html(wCount);
-			gCount = 9;
-			$("#guessCount").html(gCount);	
-		}
-	}//function closing bracket	
+				if (gCount <= 0) {
+					lCount++;
+					$("#lossCount").html(lCount);
+					gCount = 9;
+					$("#guessCount").html(gCount);
+					alert("YOU LOSE! Start Over.");
+				}else {
+					gCount--;
+					$("#guessCount").html(gCount);
+					
+				}	
+			}else {
+				alert("YOU WIN!");				
+				wCount++;
+				$("#winCount").html(wCount);
+				gCount = 9;
+				$("#guessCount").html(gCount);	
+			}
+		}//function closing bracket	
+	}	
 });
