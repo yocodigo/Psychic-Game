eb5afde2ec5e49ab5ab9bbf71413c0662f99c3d8
unknown <jjhernandez94@gmail.com>
2018-06-12 19:33:22 -0700

    Added some comments and completed jQuery conversion

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,49 @@
 $(document).ready(function() {
-	var gameStatus = false;
 	
-	$(".jumbotron").click(function() {
-		gameStatus = true;
-		$("#display-4").html("Enter any letter");
-		
-	});	
-	debugger;
-	console.log(gameStatus);
-	if(gameStatus) {
-		runGame();
-	}
-	
-	//Initial counts
+	// Initial counts
 	let wCount = 0;
 	let lCount = 0;
 	let gCount = 9;
-	let computerGuess = $("pcGuess");
-	let userText = $("guess");
 	
-	//Display initial counts
+	// Display initial counts
 	$("#winCount").html(wCount);
 	$("#lossCount").html(lCount);
 	$("#guessCount").html(gCount);
+
+	// User Selection Process
+	// When I press a button(onkeyup), trigger the following events (using function(event)...
+	$(document).keyup(function(event) {
+		let userGuess = event.key;
+		$("#guess").html(userGuess);
 		
-	function runGame() {
+		// Computer Selection Process
+		let computerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		let computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
-		$(".game").attr("visibility", "visible");	
+		// Compare user and machine selections
+		if (userGuess != computerGuess) {
 
-		// User Selection Process
-		//When I press a button(onkeyup), trigger the following events (using function(event)...
-		document.onkeyup = function(event) {
-			
-			userText.textContent = event.key;
-			let userGuess = event.key;
-			
-			//Computer Selection Process
-			let computerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-			let computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-			computerGuess.textContent;
-
-			//Compare user and computer selections
-			if (userGuess != computerGuess) {
-				
-				if (gCount <= 0) {
-					lCount++;
-					$("#lossCount").html(lCount);
-					gCount = 9;
-					$("#guessCount").html(gCount);
-					alert("YOU LOSE! Start Over.");
-				}else {
-					gCount--;
-					$("#guessCount").html(gCount);
-					
-				}	
-			}else {
-				alert("YOU WIN!");				
-				wCount++;
-				$("#winCount").html(wCount);
+			// User loses if all 9 guesses used up
+			if (gCount <= 0) {
+				lCount++;
+				$("#lossCount").html(lCount);
 				gCount = 9;
-				$("#guessCount").html(gCount);	
-			}
-		}//function closing bracket	
-	}	
+				$("#guessCount").html(gCount);
+				alert("YOU LOSE! Start Over.");
+			}else {
+				// Subtract one guess
+				gCount--;
+				$("#guessCount").html(gCount);
+				
+			}	
+		}else {
+			// user's guess matches machine guess
+			alert("YOU WIN!");				
+			wCount++;
+			$("#winCount").html(wCount);
+			gCount = 9;
+			$("#guessCount").html(gCount);	
+		}
+	});// function closing bracket	
+	
 });
